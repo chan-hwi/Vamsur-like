b5908a5e5e976f4555faf4d3c44a4a96bef63931
chan-hwi <gazebo5@naver.com>
2023-05-10 23:04:46 +0900

    feat: basic player control and animation

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    private Vector2 inputVec;
+    private Rigidbody2D rigid;
+    private SpriteRenderer spRenderer;
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(rigid.position + inputVec * speed * Time.fixedDeltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        if (inputVec.x != 0)
+        {
+            spRenderer.flipX = inputVec.x < 0;
+        }
+        anim.SetFloat("speed_f", inputVec.magnitude);
+    }
+}
